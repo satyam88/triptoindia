f8f07baee3363934c9f9594b14945e633ae1bd20
satyam pandey <satyampandey@192.168.1.3>
2025-11-16 08:28:12 +0530

    coderefactor

---
+++ src/main/java/com/example/triptoindia/TriptoindiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.triptoindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptoindiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptoindiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/triptoindia/TriptoindiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triptoindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptoindiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
